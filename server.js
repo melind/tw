b108e38f4cffae38b2fb5746e66e4913b2fd6471
Mélinda M <melinda93@hotmail.fr>
2019-12-29 20:39:45 +0100

    prod

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ function run() {
                             return;
                         }
                         // lancer l'appli
-                        app.listen('https://watchto.herokuapp.com/', function () {
+                        app.listen(`https://watchto.herokuapp.com/${PORT}`, function () {
                             
                         });
                     })];
