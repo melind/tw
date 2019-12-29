86ab91152039826debeb4817e0fb9cef8878a31b
Mélinda M <melinda93@hotmail.fr>
2019-12-29 17:03:32 +0100

    prod

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use(expressSession({
     resave: true, //to tell session is still active(update) even isn't modified
     saveUninitialized: false,//not store in session store if session isn't modified for such time(delete session)
     secret: 'melimelo', //key used for encrypting cookies
-  store: new MongoStore(options)
+  store: new MongoStore()
 }));
 app.use(cors({
     "origin": [URL_CORS, URL_CORS_TWO],
