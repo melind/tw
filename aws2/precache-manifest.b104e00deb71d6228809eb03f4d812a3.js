b957206f63e6f38aa00946d6c4857b5d7fcf9c13
Mélinda M <melinda93@hotmail.fr>
2020-01-21 13:03:16 +0100

    aws

---
+++ aws2/precache-manifest.b104e00deb71d6228809eb03f4d812a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2188973f16d2b713eb0393dcea52ace7",
+    "url": "/tw/index.html"
+  },
+  {
+    "revision": "2164c228899611e1f865",
+    "url": "/tw/static/css/2.fb9ed26a.chunk.css"
+  },
+  {
+    "revision": "e1a9d8e42bb3ccda6774",
+    "url": "/tw/static/css/main.fdfe801d.chunk.css"
+  },
+  {
+    "revision": "2164c228899611e1f865",
+    "url": "/tw/static/js/2.a2fe0688.chunk.js"
+  },
+  {
+    "revision": "e1a9d8e42bb3ccda6774",
+    "url": "/tw/static/js/main.43915137.chunk.js"
+  },
+  {
+    "revision": "8e1f6900875c41d25ff9",
+    "url": "/tw/static/js/runtime-main.67f5bed7.js"
+  }
+]);
