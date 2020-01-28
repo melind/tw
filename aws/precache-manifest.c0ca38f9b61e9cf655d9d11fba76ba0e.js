1f1da217ba4e9638fe56bda0ae69766b25666e36
Mélinda M <melinda93@hotmail.fr>
2020-01-28 20:03:17 +0100

    aws deploy

---
+++ aws/precache-manifest.c0ca38f9b61e9cf655d9d11fba76ba0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3edb7672b85dc601d8989bbc2768da71",
+    "url": "/tw/index.html"
+  },
+  {
+    "revision": "8faf00196f0f8bfe9d21",
+    "url": "/tw/static/css/2.fb9ed26a.chunk.css"
+  },
+  {
+    "revision": "cb4623629fafe9e49b03",
+    "url": "/tw/static/css/main.45cf7b6a.chunk.css"
+  },
+  {
+    "revision": "8faf00196f0f8bfe9d21",
+    "url": "/tw/static/js/2.7a6f7d3f.chunk.js"
+  },
+  {
+    "revision": "cb4623629fafe9e49b03",
+    "url": "/tw/static/js/main.7a6adea2.chunk.js"
+  },
+  {
+    "revision": "8e1f6900875c41d25ff9",
+    "url": "/tw/static/js/runtime-main.67f5bed7.js"
+  }
+]);
---
+++ aws/precache-manifest.e34f08cef26d9279c6fff0ca49769108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1234303ff34438651ba064b10412f46",
+    "url": "/tw/index.html"
+  },
+  {
+    "revision": "d0d771d52f9334db7a56",
+    "url": "/tw/static/css/2.fb9ed26a.chunk.css"
+  },
+  {
+    "revision": "a101e25bfbae2933ee12",
+    "url": "/tw/static/css/main.fdfe801d.chunk.css"
+  },
+  {
+    "revision": "d0d771d52f9334db7a56",
+    "url": "/tw/static/js/2.19e03eb3.chunk.js"
+  },
+  {
+    "revision": "a101e25bfbae2933ee12",
+    "url": "/tw/static/js/main.b6bf1736.chunk.js"
+  },
+  {
+    "revision": "8e1f6900875c41d25ff9",
+    "url": "/tw/static/js/runtime-main.67f5bed7.js"
+  }
+]);
